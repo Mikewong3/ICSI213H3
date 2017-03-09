056ccbbbc4767191a7e490c75db28b319fff0049
Mike <Mike@169.226.246.217>
2017-03-09 13:53:58 -0500

    added get/set/toString methods

--- ICSI213H3/src/StringNode.java
+++ ICSI213H3/src/StringNode.java
@@ -1,7 +1,40 @@
 
 public class StringNode 
 {
-	private String name; 
+	private String data; 
 	private int id; 
+	private StringNode next; 
+	
+	public StringNode(String data,int id)
+	{
+		this.data=data; 
+		this.id=id; 
+		next = null; 
+	}
+	
+	public String getString()
+	{
+		return data; 
+	}
+	public int getID()
+	{
+		return id; 
+	}
+	public StringNode getNext()
+	{
+		return next; 
+	}
+	public void setString(String n)
+	{
+		data = n; 
+	}
+	public void setID(int i)
+	{
+		id = i; 
+	}
+	public String toString()
+	{
+		return this.getID()+ ": " + this.getString();
+	}
 
 }
